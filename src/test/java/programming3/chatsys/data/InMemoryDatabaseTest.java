f57457fe3f3a50bc0632cb3c420f6add882bb6a6
Flypaper <zhang852065866@gmail.com>
2021-10-17 20:53:17 +0800

    new

---
+++ src/test/java/programming3/chatsys/data/InMemoryDatabaseTest.java
@@ -0,0 +1,18 @@
+package programming3.chatsys.data;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InMemoryDatabaseTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void authenticate() {
+    }
+}
