5f14b59b2c2f36a6047f4695f816ed7eabeea071
852065866@qq.com <zzy2000928>
2021-10-18 15:18:39 +0800

    ChatSys

--- src/test/java/programming3/chatsys/data/InMemoryDatabaseTest.java
+++ src/test/java/programming3/chatsys/data/InMemoryDatabaseTest.java
@@ -3,16 +3,29 @@ package programming3.chatsys.data;
 import static org.junit.jupiter.api.Assertions.*;
 
 class InMemoryDatabaseTest {
+    private InMemoryDatabase db;
 
     @org.junit.jupiter.api.BeforeEach
     void setUp() {
+        db = new InMemoryDatabase();
+        db.register(new User("zhang","ZhangZeyang","password"));
     }
 
     @org.junit.jupiter.api.AfterEach
     void tearDown() {
+        db.close();
+
     }
 
     @org.junit.jupiter.api.Test
-    void authenticate() {
+    void testauthenticate() {
+        assertTrue(db.authenticate("zhang","password"));
+        assertFalse(db.authenticate("zhang","not"));
+        assertFalse(db.authenticate("wang","password"));
+    }
+    @org.junit.jupiter.api.Test
+    void testnewdatabase(){
+        assertEquals(1,db.getNumberUsers());
+        assertEquals(1,db.getNumberMessages());
     }
 }
